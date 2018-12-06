c1c50114ff51301493399d5e8f212cca0b323143
Leo Sunmo <leo@vendhq.com>
2018-12-07 10:52:25 +1300

    Updates examples

--- example/main.go
+++ example/main.go
@@ -7,22 +7,18 @@ import (
 	"github.com/leosunmo/goluxafor"
 )
 
-func randomColour() uint8 {
-	return uint8(rand.Intn(0xff))
-}
-
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	luxafor := goluxafor.NewLuxafor()
-	// luxafor.Strobe(goluxafor.LedAll, randomColour(), randomColour(), randomColour(), 10, 2)
+	// luxafor.Strobe(goluxafor.LedAll, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 10, 2)
 	luxafor.Wave(goluxafor.Wave1, 255, 0, 255, 1, 10)
 	// luxafor.Pattern(goluxafor.Pattern4, 0)
-	// luxafor.Colour(goluxafor.Led1, randomColour(), randomColour(), randomColour(), 0)
-	// luxafor.Colour(goluxafor.Led2, randomColour(), randomColour(), randomColour(), 0)
-	// luxafor.Colour(goluxafor.Led3, randomColour(), randomColour(), randomColour(), 0)
-	// luxafor.Colour(goluxafor.Led4, randomColour(), randomColour(), randomColour(), 0)
-	// luxafor.Colour(goluxafor.Led5, randomColour(), randomColour(), randomColour(), 0)
-	// luxafor.Colour(goluxafor.Led6, randomColour(), randomColour(), randomColour(), 0)
+	// luxafor.Colour(goluxafor.Led1, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 0)
+	// luxafor.Colour(goluxafor.Led2, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 0)
+	// luxafor.Colour(goluxafor.Led3, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 0)
+	// luxafor.Colour(goluxafor.Led4, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 0)
+	// luxafor.Colour(goluxafor.Led5, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 0)
+	// luxafor.Colour(goluxafor.Led6, goluxafor.RandomColour(), goluxafor.RandomColour(), goluxafor.RandomColour(), 0)
 	// luxafor.Colour(goluxafor.Led1, 0, 200, 0, 0)
 	// luxafor.Colour(goluxafor.Led2, 0, 0, 0, 0)
 	// luxafor.Colour(goluxafor.Led3, 255, 0, 0, 0)
